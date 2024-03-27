76bc588634896280c5a2b91345d62950b09187c2
Nigel Seecharan <44081182+nseecharan@users.noreply.github.com>
2024-03-27 01:35:29 +0000

    ch 1 complete

---
+++ front-end/src/pages/ShoppingCartPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Shopping Cart</h1>
+  <div v-if="cartItems.length > 0">
+    <ShoppingCartList :products="cartItems" />
+    <button class="checkout-button">Proceed to Checkout</button>
+  </div>
+  <div v-if="cartItems.length === 0">
+    You currently have no items in your cart
+  </div>
+</template>
+
+<script>
+import ShoppingCartList from "../components/ShoppingCartList.vue";
+import { cartItems } from "../temp-data";
+export default {
+  name: "ShoppingCartPage",
+  data() {
+    return {
+      cartItems,
+    };
+  },
+  components: {
+    ShoppingCartList,
+  },
+};
+</script>
